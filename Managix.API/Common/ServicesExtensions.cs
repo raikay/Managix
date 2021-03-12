349ba2e51d537ace75e33d099e92ffe1414b7f64
Raikay <raikay@163.com>
2021-03-12 15:53:50 +0800

    修改Common

--- Managix.API/Common/ServicesExtensions.cs
+++ Managix.API/Common/ServicesExtensions.cs
@@ -192,7 +192,7 @@ namespace Managix.API.Common
 
                 options.IncludeXmlComments(Path.Combine(basePath, "Managix.API.xml"), true);
 
-                options.IncludeXmlComments(Path.Combine(basePath, "Managix.Core.xml"), true);
+                options.IncludeXmlComments(Path.Combine(basePath, "Managix.Common.xml"), true);
 
                 options.IncludeXmlComments(Path.Combine(basePath, "Managix.IServices.xml"));
 
